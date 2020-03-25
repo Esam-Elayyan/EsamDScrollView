8c47870885fad66039b5bf9bdc323e249328683b
Hash Techs <hashtechsdevteam@gmail.com>
2020-03-25 12:57:14 +0200

    0.1.0

--- Example/EsamDScrollView/ViewController.swift
+++ Example/EsamDScrollView/ViewController.swift
@@ -7,18 +7,34 @@
 //
 
 import UIKit
+import EsamDScrollView
 
 class ViewController: UIViewController {
 
+    let scrollView = DScrollView()
+    let scrollViewContainer = DScrollViewContainer(axis: .vertical, spacing: 10)
+    let scrollViewElement0 = DScrollViewElement(height: 1200, backgroundColor: .purple)
+    let scrollViewElement1 = DScrollViewElement(height: 500, backgroundColor: .purple)//.withBackground(image: #imageLiteral(resourceName: "icon.png"), contentMode: .scaleAspectFit)
+    
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        label.text = "I am a label"
+        label.textColor = .white
+        
+        view.addScrollView(scrollView,
+                           withSafeArea: .none,
+                           hasStatusBarCover: true,
+                           statusBarBackgroundColor: .white,
+                           container: scrollViewContainer,
+                           elements: scrollViewElement0, scrollViewElement1)
+        
+        scrollViewElement1.addSubview(label)
+        label.edgeTo(scrollViewElement1)
+        //label.centerInSuperview()
     }
 
 }
-
